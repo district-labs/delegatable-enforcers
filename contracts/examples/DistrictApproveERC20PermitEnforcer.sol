d9dc44f88a4b9a55f32934a063f98c965fafd42f
gkoum-7 <80713015+gkoum-7@users.noreply.github.com>
2023-01-04 04:22:22 -0500

    create DistrictApproveERC20PermitEnforcer

---
+++ contracts/examples/DistrictApproveERC20PermitEnforcer.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import "@district-labs/uniswap-oracle/contracts/interfaces/IDistrictUniswapV3Oracle.sol";
+import { BytesLib } from "@delegatable/delegatable-sol/contracts/libraries/BytesLib.sol";
+import { CaveatEnforcer, Transaction } from "@delegatable/delegatable-sol/contracts/CaveatEnforcer.sol";
+import "@delegatable/delegatable-sol/contracts/Delegatable.sol";
+
+import "hardhat/console.sol";
+
+contract DistrictApproveERC20PermitEnforcer is CaveatEnforcer {
+
+  function enforceCaveat(
+    bytes calldata terms,
+    Transaction calldata transaction,
+    bytes32 delegationHash
+  ) public override returns (bool) {
+    bytes4 targetSig = bytes4(transaction.data[0:4]);
+    bytes4 _allowedSig = 0x3e684702;
+    require(targetSig == _allowedSig, "DistrictERC20SubscriptionsEnforcer:invalid-method");
+    return true;
+  }
+}
