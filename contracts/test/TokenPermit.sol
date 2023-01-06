3d0113d2591269285da7c66e6588dba86e881a04
kames <kamescg@gmail.com>
2023-01-06 10:31:33 -0300

    feat(deploy): add deploy script for subscriptions

--- contracts/test/TokenPermit.sol
+++ contracts/test/TokenPermit.sol
@@ -1,11 +1,11 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
 contract TokenPermit is ERC20, ERC20Permit {
-  constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {
-    _mint(msg.sender, 100000);
+  constructor() ERC20("DistrictLabs", "DSTR") ERC20Permit("MyToken") {
+    _mint(msg.sender, 100000 * 10 ** decimals());
   }
 }
---
+++ deploy/00_subscriptions.ts
@@ -0,0 +1,37 @@
+import { utils } from "ethers";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+export default async function deploy(hardhat: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hardhat;
+
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  const token = await deploy("TokenPermit", {
+    contract: "TokenPermit",
+    from: deployer,
+    args: [],
+    skipIfAlreadyDeployed: false,
+    log: true,
+  });
+
+  await deploy("DistrictERC20PermitSubscriptionsEnforcer", {
+    contract: "DistrictERC20PermitSubscriptionsEnforcer",
+    from: deployer,
+    args: [],
+    skipIfAlreadyDeployed: false,
+    log: true,
+  });
+ 
+  await deploy("VerifyingContractERC20PermitSubscriptions", {
+    contract: "VerifyingContractERC20PermitSubscriptions",
+    from: deployer,
+    args: [
+      "DistrictLabs",
+      token.address,
+      utils.parseEther("10"),
+    ],
+    skipIfAlreadyDeployed: false,
+    log: true,
+  });
+}
